83552c92daae7da36bd779e6f63a7c9d22fecf19
Fernando Daciuk <f.daciuk@gmail.com>
2016-02-08 23:09:02 -0200

    Add logical on app component, removing from main

--- preact-rollup-redux/src/main.js
+++ preact-rollup-redux/src/main.js
@@ -1,37 +1,6 @@
 'use strict'
 
-import preact, { h, Component } from 'preact'
-import { createStore } from 'redux'
-import Counter from './components/counter'
-import counter from './reducers/counter'
+import { h, render } from 'preact'
+import App from './components/app'
 
-const store = createStore(counter)
-
-class Main extends Component {
-  constructor() {
-    super()
-    this.state.value = store.getState()
-  }
-
-  onIncrement() {
-    store.dispatch({ type: 'INCREMENT' })
-    this.setState({ value: store.getState() })
-  }
-
-  onDecrement() {
-    store.dispatch({ type: 'DECREMENT' })
-    this.setState({ value: store.getState() })
-  }
-
-  render() {
-    return (
-      <Counter
-        value={this.state.value}
-        onIncrement={this.onIncrement.bind(this)}
-        onDecrement={this.onDecrement.bind(this)}
-      />
-    )
-  }
-}
-
-preact.render(<Main />, document.querySelector('[data-js="main"]'))
+render(<App />, document.querySelector('[data-js="main"]'))
