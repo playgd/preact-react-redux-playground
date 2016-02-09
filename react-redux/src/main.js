a9fb469f5344b65e9e8736e38b631aa33af4b2bf
Fernando Daciuk <f.daciuk@gmail.com>
2016-02-09 21:40:07 -0200

    Add provider by react-redux

--- react-redux/src/main.js
+++ react-redux/src/main.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
-import Provider from './components/provider'
+import { Provider } from 'react-redux'
 import App from './components/app'
 import counter from './reducers/counter'
 
