f7ea66e649c1fc53a18507773a193f2c32539cd1
Fernando Daciuk <f.daciuk@gmail.com>
2016-02-09 21:28:09 -0200

    Move logic to main.js

--- react-redux/src/main.js
+++ react-redux/src/main.js
@@ -2,6 +2,14 @@
 
 import React from 'react'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
+import Provider from './components/provider'
 import App from './components/app'
+import counter from './reducers/counter'
 
-render(<App />, document.querySelector('[data-js="main"]'))
+render(
+  <Provider store={createStore(counter)}>
+    <App />
+  </Provider>,
+  document.querySelector('[data-js="main"]')
+)
