c638e9acc9b60e7271b8b39a496436da33ed982a
Fernando Daciuk <f.daciuk@gmail.com>
2016-02-08 23:09:31 -0200

    Improve reducer adding new actions

--- preact-rollup-redux/src/reducers/counter.js
+++ preact-rollup-redux/src/reducers/counter.js
@@ -1,11 +1,40 @@
 'use strict'
 
-const counter = (state = 0, action) => {
+const counter = (state = [0], action) => {
   switch(action.type) {
-    case 'INCREMENT': return state + 1
-    case 'DECREMENT': return state - 1
+    case 'ADD_COUNTER': return addCounter(state)
+    case 'REMOVE_COUNTER': return removeCounter(state, action.index)
+    case 'INCREMENT': return incrementCounter(state, action.index)
+    case 'DECREMENT': return decrementCounter(state, action.index)
     default: return state
   }
+}
+
+const addCounter = (list) => {
+  return [...list, 0]
+}
+
+const removeCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    ...list.slice(index + 1)
+  ]
+}
+
+const incrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index + 1)
+  ]
+}
+
+const decrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index] - 1,
+    ...list.slice(index + 1)
+  ]
 }
 
 export default counter
