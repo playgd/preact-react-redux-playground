a971fced5cc017482f68fd1dc2f5ebcd28800db5
Fernando Daciuk <f.daciuk@gmail.com>
2016-02-09 12:16:14 -0200

    Add webpack config

--- react-redux/webpack.config.js
+++ react-redux/webpack.config.js
@@ -1,11 +1,32 @@
 'use strict'
-
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   entry: path.resolve('.', 'src', 'main.js'),
   output: {
     path: `${__dirname}/public`,
     filename: 'bundle.js'
-  }
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  plugins: (() => {
+    if(!process.env.UGLIFY) {
+      return []
+    }
+
+    return [
+      new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+          warnings: false
+        }
+      })
+    ]
+  })()
 }
