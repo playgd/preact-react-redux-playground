201abe77bdd318432efccb4eb9f51cd0e0d75623
Fernando Daciuk <f.daciuk@gmail.com>
2016-02-13 16:47:08 -0200

    Converte app to component

--- preact-redux-todo/src/components/app.js
+++ preact-redux-todo/src/components/app.js
@@ -1,11 +1,34 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => {
-  return (
-    <div>App!</div>
-  )
+class App extends Component {
+  componentDidMount() {
+    this.context.store.subscribe(() => this.setState({}))
+  }
+
+  render() {
+    const { store } = this.context
+    const todos = store.getState()
+
+    return (
+      <div>
+        <button onClick={() => store.dispatch({ type: 'ADD_TODO' })}>
+          Add todo
+        </button>
+
+        <ul>
+          {todos.map(todo => (
+            <li key={todo.id}>{todo.text}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+App.contextTypes = {
+  store: React.PropTypes.object
 }
 
 export default App
