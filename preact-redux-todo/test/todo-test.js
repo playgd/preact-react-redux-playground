bf35e6454b765db0f05dc3d98ed5debc70dd2390
Fernando Daciuk <f.daciuk@gmail.com>
2016-02-13 18:24:54 -0200

    Add other tests

--- preact-redux-todo/test/todo-test.js
+++ preact-redux-todo/test/todo-test.js
@@ -18,4 +18,56 @@ describe('#TODO', function() {
     }]
     expect(todo(before, action)).to.eql(after)
   })
+
+  it('Should add new todo', function() {
+    const before = Object.freeze([{
+      id: 0,
+      text: 'hey',
+      completed: false
+    }])
+    const action = Object.freeze({
+      type: 'ADD_TODO',
+      text: 'ho',
+      id: 1
+    })
+    const after = [{
+      id: 0,
+      text: 'hey',
+      completed: false
+    },{
+      id: 1,
+      text: 'ho',
+      completed: false
+    }]
+    expect(todo(before, action)).to.eql(after)
+  })
+
+  it('Should unknown action type return the current state', function() {
+    const before = Object.freeze([])
+    const action = Object.freeze({ type: 'ADD_TUDU' })
+    const after = []
+    expect(todo(before, action)).to.eql(after)
+  })
+
+  it('Should toggle todo', function() {
+    const before = Object.freeze([{
+      id: 0,
+      text: 'hey',
+      completed: false
+    },{
+      id: 1,
+      text: 'ho',
+      completed: false
+    }])
+    const action = Object.freeze({ type: 'TOGGLE_TODO', id: 1 })
+    const after = [{
+      id: 0,
+      text: 'hey',
+      completed: false
+    },{
+      id: 1,
+      text: 'ho',
+      completed: true
+    }]
+  })
 })
