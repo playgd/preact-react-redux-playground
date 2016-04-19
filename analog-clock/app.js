c00bdce44a44366320531b4fcbce1ced7bdfde11
Fernando Daciuk <f.daciuk@gmail.com>
2016-04-19 00:07:40 -0300

    Change client directory

--- analog-clock/app.js
+++ analog-clock/app.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static('client'))
 
 app.listen(3000)
 console.log('Listening on 3000 port')
