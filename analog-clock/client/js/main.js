f742e4033425a32f81338efda9492c6bba6b1242
Fernando Daciuk <f.daciuk@gmail.com>
2016-04-23 02:04:20 -0300

    Add clock2 component

--- analog-clock/client/js/main.js
+++ analog-clock/client/js/main.js
@@ -3,8 +3,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Clock from './components/clock'
+import Clock2 from './components/clock2'
 
 render(
-  <Clock />,
+  <Clock2 />,
   document.querySelector('[data-js="main"]')
 )
